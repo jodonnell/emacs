6602dbd174c02e1c79fbac5d819e5925b3745a72
Jacob O'Donnell <jodonnell@cyrusinnovation.com>
2013-12-05 17:42:49 -0500

    working on extract

--- .emacs.d/extract.rb
+++ .emacs.d/extract.rb
@@ -15,15 +15,39 @@ class ASTRefactor
   def get_assigned_vars ruby_ast, var, assignment=false
     next_is_var = false
     ruby_ast.each do |thing|
-      return var.push(thing) if next_is_var
+      return var.push(thing).uniq if next_is_var
       if thing.is_a? Array
         get_assigned_vars thing, var, assignment
       elsif thing == :assign
         assignment = true
       elsif thing == :@ident and assignment
         next_is_var = true
+        assignment = false
       end
     end
     return var
   end
+
+  def get_local_variables_from_caller ruby_ast, local_variables
+    ruby_ast.each_with_index do |thing, index|
+      if thing.is_a? Array
+        get_local_variables_from_caller thing, local_variables
+      elsif thing == :assign
+        return local_variables.push(ruby_ast[index + 1][1][1])
+      end
+    end
+    return local_variables
+  end
+
+  def get_variable_references ruby_ast, variable_references
+    ruby_ast.each_with_index do |thing, index|
+      if thing.is_a? Array
+        get_variable_references thing, variable_references
+      elsif thing == :var_ref and ruby_ast[index + 1][0] == :@ident
+        return variable_references.push(ruby_ast[index + 1][1])
+      end
+    end
+    return variable_references
+  end
+
 end
