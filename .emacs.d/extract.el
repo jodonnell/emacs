e5fcdd2020f55217b7240259158cecbba1ca8802
Jacob O'Donnell <jacobodonnell@gmail.com>
2014-11-03 08:12:30 -0500

    use irb instead of a rails package

--- .emacs.d/extract.el
+++ .emacs.d/extract.el
@@ -32,7 +32,7 @@
 
 (defun start-extract-process()
   (let ((process-connection-type nil))  ; use a pipe
-    (start-process-shell-command extract-process-name "yowser" "(cd /Users/jacob/programming/kickoffkit && rails c)")
+    (start-process extract-process-name "yowser" "irb")
   (set-process-filter (extract-process) 'extract-process-filter)))
 ;(delete-process (extract-process))
 
