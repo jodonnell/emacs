29a4ce5924cac1a5b31be00b858f1a1c7adf100a
Jacob O'Donnell <jacobodonnell@gmail.com>
2014-12-20 14:00:02 -0500

    removing packages from the old style deployment

--- .emacs.d/elpa/kite-20130201.2038/kite-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "kite" "20130201.2038" "WebKit inspector front-end [github]"
-  '((json "1.2")
-    (websocket "0.93.1")))
-
--- .emacs.d/elpa/zenburn-theme-20141112.923/zenburn-theme-autoloads.el
+++
@@ -1,34 +0,0 @@
-;;; zenburn-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "../../../emacs/.emacs.d/elpa/zenburn-theme-20141112.923/zenburn-theme"
-;;;;;;  "zenburn-theme.el" "e5d439f3da93c331e7df699bdb9822f1")
-;;; Generated autoloads from zenburn-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil "zenburn-theme" "../../../../.emacs.d/elpa/zenburn-theme-20141112.923/zenburn-theme.el"
-;;;;;;  "e5d439f3da93c331e7df699bdb9822f1")
-;;; Generated autoloads from ../../../../.emacs.d/elpa/zenburn-theme-20141112.923/zenburn-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/zenburn-theme-20141112.923/zenburn-theme-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/zenburn-theme-20141112.923/zenburn-theme-pkg.el"
-;;;;;;  "../../../../.emacs.d/elpa/zenburn-theme-20141112.923/zenburn-theme.el"
-;;;;;;  "zenburn-theme.el") (21652 62635 835566 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; zenburn-theme-autoloads.el ends here
